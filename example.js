77663f2e49d4b3e7e641c28b8df3495569441729
amanda-posey <amanda.posey@live.com>
2021-04-27 16:28:32 -0400

    drop back 10 and punt

--- example.js
+++ example.js
@@ -1,6 +1,3 @@
-console.log('Super dope game');
-
-// TODO: Create an issue
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -8,7 +5,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {y: 300},
+            gravity: { y: 300 },
             debug: false
         }
     },
@@ -18,11 +15,19 @@ var config = {
         update: update
     }
 };
-// 800x600 sets the resolution of our game.
+
+var player;
+var stars;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+var gameOver = false;
+var scoreText;
+
 var game = new Phaser.Game(config);
 
 function preload ()
-// This is where the images and such needed for the game will preload.
 {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
@@ -31,82 +36,151 @@ function preload ()
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
-var platforms;
-var cursors;
-
 function create ()
-// Game objects live here.
-// 400, 300 tells the image where to display on the canvas. This is the center.
 {
+    //  A simple background for our game
     this.add.image(400, 300, 'sky');
-    
-    //This command makes physics not apply to the platforms, as they are static.
+
+    //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = this.physics.add.staticGroup();
 
-    // The following adds a base "ground" element, and some ledges.
+    //  Here we create the ground.
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
+    //  Now let's create some ledges
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
-    platforms.create(750, 200, 'ground');
+    platforms.create(750, 220, 'ground');
 
-    // Now we add our player. His name is Dude. 
-    player = this.physics.add.sprite(100, 450, 'dude'); //creates sprite called player 100x450 pixels from bottom
+    // The player and its settings
+    player = this.physics.add.sprite(100, 450, 'dude');
 
+    //  Player physics properties. Give the little guy a slight bounce.
     player.setBounce(0.2);
-    player.setCollideWorldBounds(true); //so Dude doesn't fall off the map
+    player.setCollideWorldBounds(true);
 
-    //Sprite sheet has 9 frames - these tell the computer which to use during turns.
+    //  Our player animations, turning, walking left and walking right.
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
         key: 'turn',
-        frames: [ {key:'dude', frame: 4} ],
+        frames: [ { key: 'dude', frame: 4 } ],
         frameRate: 20
     });
 
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end:8 }),
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1
     });
 
-    cursors = this.input.keyboard.createCursorKeys(); //Phaser uses this to eliminate the need for event listeners.
-    // The rest of the instructions for moving will be in the update function.
+    //  Input Events
+    cursors = this.input.keyboard.createCursorKeys();
 
-    this.physics.add.collider(player, platforms); //keeps Dude from falling through the ground. Super important.
+    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(function (child) {
+
+        //  Give each star a slightly different bounce
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+    });
+
+    bombs = this.physics.add.group();
+
+    //  The score
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+    //  Collide the player and the stars with the platforms
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(bombs, platforms);
+
+    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 function update ()
-{ // in which I try to explain movement.
-
-    if (cursors.left.isDown) // if the left cursor button is pressed
 {
-    player.setVelocityX(-160); // Dude moves at 160px/sec on the x axis, to the left.
+    if (gameOver)
+    {
+        return;
+    }
 
-    player.anims.play('left', true); // This sets which images from the sprite sheet get used, from the Create function
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
 }
-else if (cursors.right.isDown) // This statement is the exact opposite of the one for left.
-{
-    player.setVelocityX(160);
 
-    player.anims.play('right', true);
-}
-else
+function collectStar (player, star)
 {
-    player.setVelocityX(0); // This stops Dude when the user releases the key.
+    star.disableBody(true, true);
+
+    //  Add and update the score
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0)
+    {
+        //  A new batch of stars to collect
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+
+    }
+}
+
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
 
     player.anims.play('turn');
-}
 
-if (cursors.up.isDown && player.body.touching.down) // If up cursor is pressed AND Dude is on the ground...
-{
-    player.setVelocityY(-330); // Dude moves 330px/sec upward on his y axis. AKA, he jumps.
-}
+    gameOver = true;
 }
