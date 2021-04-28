54ada3558c7b551dba0d6fe9b02ecec4aec5729c
amanda-posey <amanda.posey@live.com>
2021-04-28 10:22:06 -0400

    scrollling background works

--- game.js
+++ game.js
@@ -1,156 +1,92 @@
-const context = document.querySelector("canvas").getContext("2d");
+var myGamePiece;
+var myBackground;
 
-context.canvas.height = 400;
-context.canvas.width = 1220;
 
-// Start the frame count at 1
-let frameCount = 1;
-// Set the number of obstacles to match the current "level"
-let obCount = frameCount;
-// Create a collection to hold the generated x coordinates
-const obXCoors = [];
-
-const square = {
-
-  height: 32,
-  jumping: true,
-  width: 32,
-  x: 0,
-  xVelocity: 0,
-  y: 0,
-  yVelocity: 0
-
-};
-
-// Create the obstacles for each frame
-const nextFrame = () => {
-  // increase the frame / "level" count
-  frameCount++;
-  
-  for (let i = 0; i < obCount; i++) {
-    // Randomly generate the x coordinate for the top corner start of the triangles
-    obXCoor = Math.floor(Math.random() * (1165 - 140 + 1) + 140);
-    obXCoors.push(obXCoor);
-  }
-
+function startGame() {
+    myGamePiece = new component(150, 150, "assets/granny.png", 10, 240, "image");
+    myBackground = new component(800, 480, "assets/bg2.png", 0, 0, "background");
+    
+    myGameArea.start();
 }
 
-const controller = {
-
-  left: false,
-  right: false,
-  up: false,
-  keyListener: function (event) {
-
-    var key_state = (event.type == "keydown") ? true : false;
-
-    switch (event.keyCode) {
-
-      case 37:// left key
-        controller.left = key_state;
-        break;
-      case 38:// up key
-        controller.up = key_state;
-        break;
-      case 39:// right key
-        controller.right = key_state;
-        break;
-
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 800;
+        this.canvas.height = 480;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
     }
+}
 
-  }
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image" || type == "background") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (type == "image" || type == "background") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        if (type == "background") {
+            ctx.drawImage(this.image, 
+                this.x + this.width, 
+                this.y,
+                this.width, this.height);
+        }
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.type == "background") {
+            if (this.x == -(this.width)) {
+                this.x = 0;
+            }
+        }
+    }    
+}
 
-};
+function updateGameArea() {
+    myGameArea.clear();
+    myBackground.speedX = -1;
+    myBackground.newPos();    
+    myBackground.update();
+    myGamePiece.newPos();    
+    myGamePiece.update();
+}
 
-const loop = function () {
+// function move(dir) {
+//     myGamePiece.image.src = "angry.gif";
+//     if (dir == "up") {myGamePiece.speedY = -1; }
+//     if (dir == "down") {myGamePiece.speedY = 1; }
+//     if (dir == "left") {myGamePiece.speedX = -1; }
+//     if (dir == "right") {myGamePiece.speedX = 1; }
+// }
 
-  if (controller.up && square.jumping == false) {
-
-    square.yVelocity -= 20;
-    square.jumping = true;
-
-  }
-
-  if (controller.left) {
-
-    square.xVelocity -= 0.5;
-
-  }
-
-  if (controller.right) {
-
-    square.xVelocity += 0.5;
-
-  }
-
-  square.yVelocity += 1.5;// gravity
-  square.x += square.xVelocity;
-  square.y += square.yVelocity;
-  square.xVelocity *= 0.9;// friction
-  square.yVelocity *= 0.9;// friction
-
-  // if square is falling below floor line
-  if (square.y > 386 - 16 - 32) {
-
-    square.jumping = false;
-    square.y = 386 - 16 - 32;
-    square.yVelocity = 0;
-
-  }
-
-  // if square is going off the left of the screen
-  if (square.x < -20) {
-
-    square.x = 1220;
-
-  } else if (square.x > 1220) {// if square goes past right boundary
-
-    square.x = -20;
-    nextFrame();
-
-  }
-  // Creates the backdrop for each frame
-  context.fillStyle = "#201A23";
-  context.fillRect(0, 0, 1220, 400); // x, y, width, height
-
-
-  // Creates and fills the cube for each frame
-  context.fillStyle = "#8DAA9D"; // hex for cube color
-  context.beginPath();
-  context.rect(square.x, square.y, square.width, square.height);
-  context.fill();
-
-
-  // Create the obstacles for each frame
-  // Set the standard obstacle height
-  const height = 200 * Math.cos(Math.PI / 6);
-
-  context.fillStyle = "#FBF5F3"; // hex for triangle color
-  obXCoors.forEach((obXCoor) => {
-    context.beginPath();
-
-    context.moveTo(obXCoor, 385); // x = random, y = coor. on "ground"
-    context.lineTo(obXCoor + 20, 385); // x = ^random + 20, y = coor. on "ground"
-    context.lineTo(obXCoor + 10, 510 - height); // x = ^random + 10, y = peak of triangle
-  
-    context.closePath();
-    context.fill();
-  })
-
-
-  // Creates the "ground" for each frame
-  context.strokeStyle = "#2E2532";
-  context.lineWidth = 30;
-  context.beginPath();
-  context.moveTo(0, 385);
-  context.lineTo(1220, 385);
-  context.stroke();
-
-  // call update when the browser is ready to draw again
-  window.requestAnimationFrame(loop);
-
-};
-
-window.addEventListener("keydown", controller.keyListener)
-window.addEventListener("keyup", controller.keyListener);
-window.requestAnimationFrame(loop);
+// function clearmove() {
+//     myGamePiece.image.src = "smiley.gif";
+//     myGamePiece.speedX = 0; 
+//     myGamePiece.speedY = 0; 
+//}
